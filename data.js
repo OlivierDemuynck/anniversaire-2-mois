414265fec4d1f5ca7e790935a4e7924eeaca50b9
Nusuma <olivier.demuynck.eic@gmail.com>
2026-09-13 15:11:24 +0200

    Add new entry with date range and subtitle

--- data.js
+++ data.js
@@ -177,6 +177,7 @@ window.SITE_DATA = {
       secretMessage: "Le 24 sera la première fois que nos yeux se rencontreront vraiment,<br>mais crois-moi, l'idée de pouvoir enfin te serrer dans mes bras <br> me chamboule déjà...",
       photos: []
     },
+     
     {
       number: 3,
       dateRange: "13 aout → 13 septembre 2026",
@@ -246,8 +247,7 @@ window.SITE_DATA = {
       ],
       secretMessage: "Peu importe la distance,<br>il y aura toujours un endroit<br>où je penserai à toi. ❤",
       photos: [] // pas de photo pour le mois 1
-    },
-
+    }
   ]
 };
 
